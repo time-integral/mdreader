95b8d0ddcbe63a07e6b78fe1ceb6a71ec90ec3fe
coder <coder@timeintegral.ai>
2026-01-03 15:18:18 -0800

    update tests

--- tests/test_fmi2.py
+++ tests/test_fmi2.py
@@ -32,18 +32,111 @@ def reference_fmus_dir(tmp_path_factory):
 @pytest.mark.parametrize(
     "reference_fmu, expected_metadata",
     [
-        ("2.0/Feedthrough.fmu", ("2.0", "Feedthrough")),
-        ("2.0/BouncingBall.fmu", ("2.0", "BouncingBall")),
-        ("2.0/VanDerPol.fmu", ("2.0", "VanDerPol")),
-        ("2.0/Dahlquist.fmu", ("2.0", "Dahlquist")),
-        ("2.0/Stair.fmu", ("2.0", "Stair")),
-        ("2.0/Resource.fmu", ("2.0", "Resource")),
+        (
+            "2.0/Feedthrough.fmu",
+            {
+                "fmi_version": "2.0",
+                "model_name": "Feedthrough",
+                "guid": "{37B954F1-CC86-4D8F-B97F-C7C36F6670D2}",
+                "description": "A model to test different variable types, causalities, and variabilities",
+                "author": None,
+                "version": None,
+                "copyright": None,
+                "license": None,
+                "generation_tool": "Reference FMUs (v0.0.38)",
+                "generation_date_and_time": "2025-02-04T04:14:59.281054+00:00",
+            },
+        ),
+        (
+            "2.0/BouncingBall.fmu",
+            {
+                "fmi_version": "2.0",
+                "model_name": "BouncingBall",
+                "guid": "{1AE5E10D-9521-4DE3-80B9-D0EAAA7D5AF1}",
+                "description": "This model calculates the trajectory, over time, of a ball dropped from a height of 1 m",
+                "author": None,
+                "version": None,
+                "copyright": None,
+                "license": None,
+                "generation_tool": "Reference FMUs (v0.0.38)",
+                "generation_date_and_time": "2025-02-04T04:14:58.875645+00:00",
+            },
+        ),
+        (
+            "2.0/VanDerPol.fmu",
+            {
+                "fmi_version": "2.0",
+                "model_name": "Van der Pol oscillator",
+                "guid": "{BD403596-3166-4232-ABC2-132BDF73E644}",
+                "description": "This model implements the van der Pol oscillator",
+                "author": None,
+                "version": None,
+                "copyright": None,
+                "license": None,
+                "generation_tool": "Reference FMUs (v0.0.38)",
+                "generation_date_and_time": "2025-02-04T04:14:59.010953+00:00",
+            },
+        ),
+        (
+            "2.0/Dahlquist.fmu",
+            {
+                "fmi_version": "2.0",
+                "model_name": "Dahlquist",
+                "guid": "{221063D2-EF4A-45FE-B954-B5BFEEA9A59B}",
+                "description": "This model implements the Dahlquist test equation",
+                "author": None,
+                "version": None,
+                "copyright": None,
+                "license": None,
+                "generation_tool": "Reference FMUs (v0.0.38)",
+                "generation_date_and_time": "2025-02-04T04:14:59.489702+00:00",
+            },
+        ),
+        (
+            "2.0/Stair.fmu",
+            {
+                "fmi_version": "2.0",
+                "model_name": "Stair",
+                "guid": "{BD403596-3166-4232-ABC2-132BDF73E644}",
+                "description": "This model generates a stair signal using time events",
+                "author": None,
+                "version": None,
+                "copyright": None,
+                "license": None,
+                "generation_tool": "Reference FMUs (v0.0.38)",
+                "generation_date_and_time": "2025-02-04T04:14:59.396420+00:00",
+            },
+        ),
+        (
+            "2.0/Resource.fmu",
+            {
+                "fmi_version": "2.0",
+                "model_name": "Resource",
+                "guid": "{7b9c2114-2ce5-4076-a138-2cbc69e069e5}",
+                "description": "This model loads data from a resource file",
+                "author": None,
+                "version": None,
+                "copyright": None,
+                "license": None,
+                "generation_tool": "Reference FMUs (v0.0.38)",
+                "generation_date_and_time": "2025-02-04T04:14:59.098389+00:00",
+            },
+        ),
     ],
 )
-def test_metadata(reference_fmu, expected_fmi_version, reference_fmus_dir):
+def test_metadata(reference_fmu, expected_metadata, reference_fmus_dir):
     filename = (reference_fmus_dir / reference_fmu).absolute()
     md = read_model_description(filename)
-    assert md.fmi_version == expected_fmi_version
+    assert md.fmi_version == expected_metadata["fmi_version"]
+    assert md.model_name == expected_metadata["model_name"]
+    assert md.guid == expected_metadata["guid"]
+    assert md.description == expected_metadata["description"]
+    assert md.author == expected_metadata["author"]
+    assert md.version == expected_metadata["version"]
+    assert md.copyright == expected_metadata["copyright"]
+    assert md.license == expected_metadata["license"]
+    assert md.generation_tool == expected_metadata["generation_tool"]
+    assert md.generation_date_and_time == expected_metadata["generation_date_and_time"]
 
 
 @pytest.mark.parametrize(
@@ -88,83 +181,3 @@ def test_scarlar_variables(
     print("Output Vars:", output_vars)
     assert sorted(input_vars) == sorted(expected_inputs)
     assert sorted(output_vars) == sorted(expected_outputs)
-
-
-# @pytest.mark.parametrize("xml_file", XML_FILES, ids=[f.name for f in XML_FILES])
-# def test_parse_xml_file(xml_file):
-#     """Test parsing a single XML file"""
-#     model = read_model_description(xml_file)
-
-#     # Basic assertions
-#     assert model.model_name
-#     assert model.fmi_version
-#     assert model.model_variables
-
-#     # Validate the model using Pydantic's validation
-#     # This ensures the model can be dumped and re-validated
-#     model.model_validate(model.model_dump())
-
-
-# @pytest.mark.parametrize("xml_file", XML_FILES, ids=[f.name for f in XML_FILES])
-# def test_model_serialization(xml_file):
-#     """Test serializing the model back to dict"""
-#     model = read_model_description(xml_file)
-
-#     # Convert back to dict
-#     model_dict = model.model_dump()
-
-#     # Check consistency
-#     assert model_dict["fmi_version"] == model.fmi_version
-#     assert model_dict["model_name"] == model.model_name
-
-
-# @pytest.mark.parametrize("xml_file", XML_FILES, ids=[f.name for f in XML_FILES])
-# def test_model_details(xml_file):
-#     """Test detailed properties of the model"""
-#     model = read_model_description(xml_file)
-
-#     # Check required attributes
-#     assert model.guid is not None
-#     assert len(model.guid) > 0
-#     assert model.generation_tool is not None
-
-#     # Check Model Variables
-#     assert model.model_variables is not None
-#     assert len(model.model_variables.variables) > 0
-
-#     # Check that every variable has a name and value reference
-#     for var in model.model_variables.variables:
-#         assert var.name is not None
-#         assert var.value_reference is not None
-#         # Check that one of the types is set (Real, Integer, Boolean, String, Enumeration)
-#         assert any(
-#             [
-#                 var.real is not None,
-#                 var.integer is not None,
-#                 var.boolean is not None,
-#                 var.string is not None,
-#                 var.enumeration is not None,
-#             ]
-#         )
-
-#     # Check Model Structure if present
-#     # if model.model_structure:
-#     #     if model.model_structure.outputs:
-#     #         for output in model.model_structure.outputs:
-#     #             assert output.index > 0
-
-#     #     if model.model_structure.derivatives:
-
-#     # Specific checks for BouncingBall (test1.xml)
-#     if model.model_name == "BouncingBall":
-#         # Check specific variables exist
-#         var_names = [v.name for v in model.model_variables.variables]
-#         assert "time" in var_names
-#         assert "h" in var_names
-#         assert "v" in var_names
-
-#         # Check unit definitions
-#         assert model.unit_definitions is not None
-#         unit_names = [u.name for u in model.unit_definitions.units]
-#         assert "m" in unit_names
-#         assert "m/s" in unit_names
